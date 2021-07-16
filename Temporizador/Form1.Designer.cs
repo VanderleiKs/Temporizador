b907f93981c80f398e92789fe95153f1fc37236f
Vanderlei K Silva <vanderleis47@gmail.com>
2021-07-15 21:07:35 -0300

    Create app timer

---
+++ Temporizador/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Temporizador
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblTela = new System.Windows.Forms.Label();
+            this.combHor = new System.Windows.Forms.ComboBox();
+            this.combMin = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.l = new System.Windows.Forms.Label();
+            this.combSegundos = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.Green;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnIniciar.ForeColor = System.Drawing.Color.White;
+            this.btnIniciar.Location = new System.Drawing.Point(19, 190);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(139, 54);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Crimson;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(176, 190);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(139, 54);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Parar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // lblTela
+            // 
+            this.lblTela.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTela.Font = new System.Drawing.Font("Felix Titling", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTela.ForeColor = System.Drawing.Color.Cyan;
+            this.lblTela.Location = new System.Drawing.Point(106, 76);
+            this.lblTela.Name = "lblTela";
+            this.lblTela.Size = new System.Drawing.Size(285, 82);
+            this.lblTela.TabIndex = 2;
+            this.lblTela.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // combHor
+            // 
+            this.combHor.FormattingEnabled = true;
+            this.combHor.Location = new System.Drawing.Point(108, 18);
+            this.combHor.Name = "combHor";
+            this.combHor.Size = new System.Drawing.Size(49, 23);
+            this.combHor.TabIndex = 3;
+            // 
+            // combMin
+            // 
+            this.combMin.FormattingEnabled = true;
+            this.combMin.Location = new System.Drawing.Point(240, 18);
+            this.combMin.Name = "combMin";
+            this.combMin.Size = new System.Drawing.Size(49, 23);
+            this.combMin.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hora:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Minutos:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Orange;
+            this.btnLimpar.Enabled = false;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(338, 190);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(139, 54);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // l
+            // 
+            this.l.AutoSize = true;
+            this.l.Location = new System.Drawing.Point(313, 21);
+            this.l.Name = "l";
+            this.l.Size = new System.Drawing.Size(62, 15);
+            this.l.TabIndex = 10;
+            this.l.Text = "Segundos:";
+            // 
+            // combSegundos
+            // 
+            this.combSegundos.FormattingEnabled = true;
+            this.combSegundos.Location = new System.Drawing.Point(381, 18);
+            this.combSegundos.Name = "combSegundos";
+            this.combSegundos.Size = new System.Drawing.Size(49, 23);
+            this.combSegundos.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(497, 276);
+            this.Controls.Add(this.l);
+            this.Controls.Add(this.combSegundos);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combMin);
+            this.Controls.Add(this.combHor);
+            this.Controls.Add(this.lblTela);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnIniciar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Temporizador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblTela;
+        private System.Windows.Forms.ComboBox combHor;
+        private System.Windows.Forms.ComboBox combMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label l;
+        private System.Windows.Forms.ComboBox combSegundos;
+    }
+}
+
---
+++ Temporizador/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temporizador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        CancellationTokenSource exit = new CancellationTokenSource();
+        bool runTask = true;
+
+       async void Timer()
+        {
+            runTask = true;
+            int second = (int)combSegundos.SelectedItem;
+            int minut = (int)combMin.SelectedItem;
+            int hour = (int)combHor.SelectedItem;
+            TimeSpan t = new(hour, minut, second);
+            lblTela.Text = t.ToString();
+            await Task.Delay(1000);
+            while (minut > 0 && runTask)
+            {
+                minut--;
+                second = 59;
+                while (second > -1 && runTask)
+                {
+                    t = new(hour, minut, second);
+                    lblTela.Text = t.ToString();
+                    await Task.Delay(1000);
+                    second--;
+                }
+            } 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            object[] horas = { 00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12 };
+
+            object[] minutos = {00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,
+                18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+                41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59};
+
+            object[] segundos = {00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,
+                18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+                41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59};
+
+            combHor.Items.AddRange(horas);
+            combHor.SelectedIndex = 0;
+
+            combMin.Items.AddRange(minutos);
+            combMin.SelectedIndex = 0;
+
+            combSegundos.Items.AddRange(segundos);
+            combSegundos.SelectedIndex = 0;
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            Timer();
+            btnIniciar.Enabled = false;
+            btnCancelar.Enabled = true;
+            btnLimpar.Enabled = false;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            btnCancelar.Enabled = false;
+            btnIniciar.Enabled = true;
+            btnLimpar.Enabled = true;
+            runTask = false;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lblTela.Text = "";
+            btnCancelar.Enabled = false;
+            btnIniciar.Enabled = true;
+            btnLimpar.Enabled = false;
+            combHor.SelectedIndex = 0;
+            combMin.SelectedIndex = 0;
+            combSegundos.SelectedIndex = 0;
+        }
+    }
+}
